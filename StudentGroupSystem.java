29a8213b29d7b9d67fe25c937ef60299008cc60e
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-09-09 15:36:24 +0100

    Create StudentGroupSystem.java

---
+++ StudentGroupSystem.java
@@ -0,0 +1,16 @@
+package hackathon_project;
+import java.util.ArrayList;
+
+public class StudyGroupSystem 
+{
+
+	ArrayList<StudyGroup> study_groups = new ArrayList<StudyGroup>();
+	int[] students_matrics;
+	
+	
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
