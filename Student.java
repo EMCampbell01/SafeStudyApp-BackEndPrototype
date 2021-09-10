cef000b94738ba4983eb72748de015f48a94c126
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-09-10 11:12:56 +0100

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,25 +1,51 @@
-package hackathon_project;
+package safeStudy;
 
-public class Student 
-{
+import java.util.ArrayList;
+
+/** 
+ * Student class for managing students using the app
+ * Authors - Julia Swietochowska, Euan Campbell
+ */
+public class Student{
 
 	// Student Info
-	private String first_name; 
-	private String last_name;
+	private String firstName; 
+	private String lastName;
 	private int matric;
 	
-	// List of group ID's of StudyGroups instance of student is in
-	private int[] groups;
-	
-	// Student Covid-19 Info
-	private boolean infected = false;
-	private boolean exposed = false;
+	// List of StudyGroups the student is in
+	private ArrayList<StudyGroup> groups = new ArrayList<>();
 
-	// Constructor
-	public Student(String f_name, String l_name, int mat)
-	{
-		first_name = f_name;
-		last_name  = l_name;
-		matric     = mat;
+	/**
+	 * Creates a student object
+	 * @param fName	Student's first name
+	 * @param lName	Student's last name
+	 * @param mat	Student's matriculation number
+	 */
+	public Student(String fName, String lName, int mat){
+		this.firstName = fName;
+		this.lastName = lName;
+		this.matric = mat;
+	}
+		
+	/** 
+	 * Returns the list of study groups the student is in
+	 * @return	an ArrayList of StudyGroup objects
+	 */
+	public ArrayList<StudyGroup> getGroups(){
+		return groups;
+	}
+		
+	/**
+	 * Adds a study group to the student's list
+	 * @param group	Study group object to be added to student's group list
+	 */
+	public void addGroup(StudyGroup group){
+		groups.add(group);
+	}
+
+	@Override
+	public String toString() {
+		return "\nStudent " + firstName + " " + lastName + " matric:" + matric;
 	}
 }
